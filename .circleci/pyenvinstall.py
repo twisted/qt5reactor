88828bfdb9572b7f40663ada5f51a1270f24fe9a
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-11-01 23:26:59 -0400

    pyenv update

--- .circleci/pyenvinstall.py
+++ .circleci/pyenvinstall.py
@@ -12,6 +12,7 @@ def main():
     versions = [v.strip().split('.') for v in versions if 'dev' not in v]
     versions = [v for v in versions if '.'.join(v).startswith(target)]
     version = '.'.join(max(versions))
+    subprocess.check_output(['pyenv', 'update'])
     subprocess.check_output(['pyenv', 'install', '--skip-existing', version])
     subprocess.check_output(['pyenv', 'global', version])
 
